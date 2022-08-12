586f593afcfbe0e3528d613250cd5bf2621e84e0
unknown <jimoh.iyiola11@gmail.com>
2022-08-12 15:49:04 +0100

    Added error page

--- app.js
+++ app.js
@@ -3,23 +3,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // const routes = require("./routes")
 const app = express();
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  //   console.log("In another middleware");
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='product'><button type='submit'>Add Product</button></form>"
-  );
-});
+app.use(shopRoute);
+app.use(adminRoute);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/add-product");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello From Express</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
 });
 // const server = http.createServer(app);
 // server.listen(3000);
