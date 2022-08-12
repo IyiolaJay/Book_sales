4743dbe99524227e9bd406feb0ea6d3956554021
unknown <jimoh.iyiola11@gmail.com>
2022-08-12 10:22:46 +0100

    refactoring codes

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const http = require("http");
+// const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+// const routes = require("./routes")
+const app = express();
 
-const routes = require("./routes");
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const server = http.createServer(routes);
-console.log("hammed");
+app.use("/add-product", (req, res, next) => {
+  //   console.log("In another middleware");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='product'><button type='submit'>Add Product</button></form>"
+  );
+});
 
-server.listen(3000);
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/add-product");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello From Express</h1>");
+});
+// const server = http.createServer(app);
+// server.listen(3000);
+
+app.listen(3000);
