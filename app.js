935b669c81f4e90f3cfc6be47adc98ec5be3296f
unknown <jimoh.iyiola11@gmail.com>
2022-08-13 13:35:03 +0100

    Serving HTML pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const http = require("http");
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 // const routes = require("./routes")
 const app = express();
@@ -9,10 +10,10 @@ const shopRoute = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(shopRoute);
-app.use(adminRoute);
+app.use("/admin", adminRoute);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 // const server = http.createServer(app);
 // server.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const path = require("path");
 
 router.use("/add-product", (req, res, next) => {
   //   console.log("In another middleware");
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='product'><button type='submit'>Add Product</button></form>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
